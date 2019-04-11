8f2f213d381e206c3cd6d4e198f963f3c6ec147c
caseyyee <caseyyee.ca@gmail.com>
2019-04-11 11:33:01 -0700

    Build

---
+++ docs/_next/static/webpack/static/development/pages/index.js.515e2f29af2f5d27dbc1.hot-update.js
@@ -0,0 +1,54 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/theme.js":
+/*!************************!*\
+  !*** ./pages/theme.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ({
+  breakpoints: ["540px", "900px"],
+  // fontSizes: ["0.8rem", "1.5rem", "1.5rem", "2.5rem", "4.7rem", "6.7rem"],
+  fontSizes: {
+    title: "4.7rem",
+    titleNum: "6.7rem",
+    headline: "1.6rem",
+    heading1: "3rem",
+    heading2: "1.8rem",
+    heading3: "1.6rem",
+    copy: "0.8rem",
+    location: "0.6rem"
+  },
+  lineHeights: {
+    headline: "2.5rem",
+    heading1: "3rem",
+    heading2: "2rem",
+    heading3: "2rem",
+    copy: "1.5rem",
+    location: "0.8rem"
+  },
+  // lineHeights: ["0.8rem", "1.5rem", "2.3rem", "2.5rem", "4.7rem"],
+  colors: {
+    copper: "#8B634A",
+    gray: "#8C8C8C"
+  },
+  // space: [
+  //   0, 4, 8, 16, 32, 64, 128, 256
+  // ],
+  fonts: {
+    serif: "vinter, serif",
+    sans: "gotham, sans-serif" // shadows: {
+    //   small: '0 0 4px rgba(0, 0, 0, .125)',
+    //   large: '0 0 24px rgba(0, 0, 0, .125)'
+    // }
+
+  }
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.515e2f29af2f5d27dbc1.hot-update.js.map
