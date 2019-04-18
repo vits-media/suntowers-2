4b82e26ef043664baa6894d6dba814faa6d48581
caseyyee <caseyyee.ca@gmail.com>
2019-04-17 23:49:09 -0700

    Removes static builds

--- docs/_next/static/7w7nKLUb4Ba63M45N0HZi/pages/theme.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["6c1a"],{"76wL":function(w,n,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/theme",function(){var w=o("32Iw");return{page:w.default||w}}])}},[["76wL","5d41","9da1"]]]);
