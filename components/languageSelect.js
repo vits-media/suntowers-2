544d7d2218328f5222e7b9b3fbc38c2615c33c9e
caseyyee <caseyyee.ca@gmail.com>
2019-04-19 10:54:16 -0700

    Clearer order

--- components/languageSelect.js
+++ components/languageSelect.js
@@ -1,6 +1,17 @@
 import i18n from "i18next";
 import { Text } from "rebass";
 
+const languages = [
+  {
+    code: "en",
+    string: "EN"
+  },
+  {
+    code: "zh",
+    string: "中文"
+  }
+];
+
 const LanguageButton = ({ lang, ...props }) => {
   const activeColor = lang === i18n.language ? "white" : "copper";
   return (
@@ -20,17 +31,6 @@ const LanguageButton = ({ lang, ...props }) => {
 };
 
 const LanguageSelect = props => {
-  const languages = [
-    {
-      code: "en",
-      string: "EN"
-    },
-    {
-      code: "zh",
-      string: "中文"
-    }
-  ];
-
   const changeLanguage = lng => i18n.changeLanguage(lng);
 
   const languageItems = languages.map(({ code, string }, index) => (
